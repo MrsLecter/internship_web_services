16b67f1d57373ab3183d7f945a82cdb435c5ef24
MrsLecter <MissisLecter@protonmail.com>
2022-08-01 13:23:47 +0300

    add first task

---
+++ 01_First_step/README.md
@@ -0,0 +1,6 @@
+** Hello World AWS
+*** Description
+<p>Use: Lambda Functions(myFibFunc, myFuncHelloWorld), API Gateway
+<p>Endpoints:</p>
+<p><code>https://[secret].amazonaws.com/hello?name=[yourName]</code> - return responce `Hello ${name}`</p>
+<p><code>https://[secret].amazonaws.com/fibanacci</code> - return first 10 fibonacci numbers</p>
---
+++ 01_First_step/myFibFunc.js
@@ -0,0 +1,19 @@
+exports.handler = async (event) => {
+    
+    function GenerateFibonacci(){
+        const fibonacci = [];
+        fibonacci[0] = 0;
+        fibonacci[1] = 1;
+        for (var i = 2; i < 10; i++) {
+          fibonacci[i] = fibonacci[i - 2] + fibonacci[i - 1];
+        }
+        return fibonacci.toString();
+    }
+    
+    
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('First 10 Fibonacci numbers: ' + GenerateFibonacci()),
+    };
+    return response;
+};

27fce52f2c6f7f2ccc4bbac3a27ac0aefa08e873
MrsLecter <MissisLecter@protonmail.com>
2022-09-19 11:21:48 +0300

    style: add eslint rules

--- 02_Template/aws-node-ts/src/functions/hello/handler.ts
+++ 02_Template/aws-node-ts/src/functions/hello/handler.ts
@@ -1,30 +1,26 @@
-import type { ValidatedEventAPIGatewayProxyEvent } from '../../libs/api-gateway';
-import { formatJSONResponse, formatJSONResponseError } from '../../libs/api-gateway';
-import { middyfy } from '../../libs/lambda';
+import type { ValidatedEventAPIGatewayProxyEvent } from "../../libs/api-gateway";
+import {
+  formatJSONResponse,
+  formatJSONResponseError,
+} from "../../libs/api-gateway";
+import { middyfy } from "../../libs/lambda";
+import schema from "./schema";
 
-import schema from './schema';
+const Boom = require("@hapi/boom");
+const Joi = require("joi");
 
-//error handler
-const Boom = require('@hapi/boom');
-//body validator
-const Joi = require('joi');
-
-//--------joi schema-----------------
 const joiSchema = Joi.object({
-  username: Joi.string()
-      .alphanum()
-      .min(3)
-      .max(30)
-      .required()
+  username: Joi.string().alphanum().min(3).max(30).required(),
 });
-//--------joi schema-----------------
 
-const hello: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
+const hello: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (
+  event,
+) => {
   const name = event.body.name;
   try {
-    const valueValid =joiSchema.validate({ username: name });
-    if(valueValid.hasOwnProperty('error')){
-      throw new Error(valueValid.error.details[0].message)
+    const valueValid = joiSchema.validate({ username: name });
+    if (valueValid.hasOwnProperty("error")) {
+      throw new Error(valueValid.error.details[0].message);
     }
     return formatJSONResponse({
       message: `Hello ${valueValid.value.username} to the exciting Serverless world!`,
@@ -32,13 +28,13 @@ const hello: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) =
     });
   } catch (err) {
     const error = Boom.badRequest((err as Error).message);
-    error.output.statusCode = 400; 
-    error.reformat(); 
+    error.output.statusCode = 400;
+    error.reformat();
     return formatJSONResponseError(
       {
         message: error,
       },
-      error.output.statusCode
+      error.output.statusCode,
     );
   }
 };
